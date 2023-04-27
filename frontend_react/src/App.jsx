96b90f6125e53fc3139111318627c05d46155baf
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-04-28 00:42:54 +0530

    #2 file & folder stucture completed

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,9 +1,22 @@
-import "./App.css";
-function App() {
+// import React from 'react'
+import "./App.scss";
+import { Navbar } from "./components";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
+const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+      </div>
     </>
   );
-}
+};
+
 export default App;
