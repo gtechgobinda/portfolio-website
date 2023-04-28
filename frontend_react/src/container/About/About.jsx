4bcd58af28a79456eff0e038333ef34d51a5f469
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-04-29 00:13:45 +0530

    #6 sanity & react connect in about section completed

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,30 +1,39 @@
 // import {useState,useEffect}from 'react'
 import { motion } from "framer-motion";
-import { images } from "../../constants";
+import { useEffect, useState } from "react";
+import { client, urlFor } from "../../client";
 import "./About.scss";
-const abouts = [
-  {
-    title: "Frontend Development",
-    description: "I am a good web developer.",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Design",
-    description: "I am a good web developer.",
-    imgUrl: images.about02,
-  },
-  {
-    title: "UI/UX",
-    description: "I am a good web developer.",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Web Animations",
-    description: "I am a good web developer.",
-    imgUrl: images.about04,
-  },
-];
+
+// const abouts = [
+//   {
+//     title: "Frontend Development",
+//     description: "I am a good web developer.",
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: "Web Design",
+//     description: "I am a good web developer.",
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: "UI/UX",
+//     description: "I am a good web developer.",
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: "Web Animations",
+//     description: "I am a good web developer.",
+//     imgUrl: images.about04,
+//   },
+// ];
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type=="abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
   return (
     <>
       <h2 className="head-text">
@@ -42,7 +51,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
