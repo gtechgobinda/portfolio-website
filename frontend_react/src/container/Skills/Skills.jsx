17fe28bd4493c4b84809c1544a30744b721032dd
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-04-30 01:25:46 +0530

    #9 skill section full completed

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,34 +1,36 @@
-// import React from 'react'
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+
 import { client, urlFor } from "../../client";
-// import ReactTooltip from "react-tooltip"
-// import { AppWrap, MotionWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Skills.scss";
 
+// eslint-disable-next-line react-refresh/only-export-components
 const Skills = () => {
-  const [experience, setExperience] = useState([]);
+  const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([]);
+  const isMobile = useMediaQuery({ query: "(max-width:700px)" });
+
   useEffect(() => {
     const query = '*[_type == "experiences"]';
-    const skillsQuery = '*[_type=="skills"]';
-
-    client.fetch(query).then((data) => {
-      setExperience(data);
-    });
-    client.fetch(skillsQuery).then((data) => {
-      setSkills(data);
-    });
+    const skillsQuery = '*[_type == "skills"]';
+    client.fetch(query).then((data) => setExperiences(data));
+    client.fetch(skillsQuery).then((data) => setSkills(data));
   }, []);
   return (
     <>
-      <h2 className="head-text">Skills & Experience</h2>
+      <h2 className="head-text">
+        Skills & <span>Experience</span>
+      </h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 0.5 }}
+              transition={{ duration: 0.25 }}
               className="app__skills-item app__flex"
               key={skill.name}
             >
@@ -42,9 +44,45 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+        <motion.div className="app__skills-exp">
+          {experiences?.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works?.map((work) => (
+                  <div className="app__skills-tooltip" key={work.name}>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.25 }}
+                      data-tooltip-id={work.name}
+                      className="app__skills-exp-work"
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      place={isMobile ? "top" : "right"}
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </div>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
     </>
   );
 };
 
-export default Skills;
+// eslint-disable-next-line react-refresh/only-export-components
+export default AppWrap(
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__whitebg"
+);
